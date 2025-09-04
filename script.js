b3574f3671c9e91c9466f307ff420e089ddf98f9
Samir Sinanovic <sinanovic010@gmail.com>
2025-09-04 21:00:22 +0200

    newe

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       entry.target.classList.add('visible');
-      observer.unobserve(entry.target); // animira se samo jednom
+      observer.unobserve(entry.target);
     }
   });
 }, {
